d0d13bf3b5f5f79fb2c672596ae011faa5bab999
Ruslan San <russanemail@gmail.com>
2021-02-15 21:42:55 +0200

    Exercise 1. First Qt GUI application.

---
+++ Lab1/Lab1_widgetApp/mainwindow.cpp
@@ -0,0 +1,21 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+    , m_iCounter(0)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->label->setNum(++m_iCounter);
+}
